2d766ad9100272a1a2f006f4f96def53f6ecbe46
ujjawal sharma <112757910+Sharmaujjawal123@users.noreply.github.com>
2022-11-26 22:23:44 +0530

    list comprehension

---
+++ list comprehension.py
@@ -0,0 +1,39 @@
+a = eval(input("cost of the product: "))
+cgst = 9
+sgst = 9
+
+amo = (cgst/100)*a
+amo2 = (sgst/100)*a
+print("add cgst @ 9% ",amo)
+print("add sgst @ 9% ",amo2)
+print("total cost = ", a+am
+
+
+
+= set()
+b ={}
+c ={1, 2, 2}
+d =()
+e =(1,2,3)
+f = {1 :"harry"}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+remove
+prin'''
+'''a =str(23)
+print(a[1]
+
+
+
+a = 30
+b = 500
+c = a*b
+print("{0:.2f}".format(c))
+
+
+
+
